69039f1a98b9c8830dc3acb90a2523dd94164ecc
kaikzsu-rgb <kaikzsu@gmail.com>
2026-03-25 08:06:18 +0300

    server.js

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const sqlite3 = require('sqlite3').verbose();
+function selectRoom(id, title) {
+            currentRoom = id;
+            document.getElementById('chat-header').innerText = title;
+            document.getElementById('messages').innerHTML = ''; // Очистка при смене
+            socket.emit('get-history', id);
+        }
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { maxHttpBufferSize: 1e7 }); // Для больших фото
-const db = new sqlite3.Database('./database.db');
+        function send() {
+            const input = document.getElementById('msgInput');
+            if(!input.value.trim()) return;
 
-db.run(`CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, text TEXT, image TEXT, time TEXT)`);
+            const data = {
+                room: currentRoom,
+                user: currentUser.name,
+                text: input.value,
+                time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+            };
 
-app.use(express.static(__dirname));
+            socket.emit('message', data);
+            input.value = '';
+        }
 
-io.on('connection', (socket) => {
-    db.all("SELECT * FROM messages ORDER BY id DESC LIMIT 50", [], (err, rows) => {
-        if (!err) socket.emit('load history', rows.reverse());
-    });
-
-    socket.on('chat message', (data) => {
-        const time = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-        db.run("INSERT INTO messages (name, text, image, time) VALUES (?, ?, ?, ?)", 
-        [data.name, data.text, data.image, time], function(err) {
-            if (!err) {
-                io.emit('chat message', { id: this.lastID, name: data.name, text: data.text, image: data.image, time: time });
-            }
+        socket.on('message', (msg) => {
+            const div = document.createElement('div');
+            div.className = `msg ${msg.user === currentUser.name ? 'my' : 'bot'}`;
+            div.innerHTML = `
+                <span class="sender">${msg.user}</span>
+                ${msg.text}
+                <div style="font-size:10px; text-align:right; opacity:0.6;">${msg.time}</div>
+            `;
+            const box = document.getElementById('messages');
+            box.appendChild(div);
+            box.scrollTop = box.scrollHeight;
         });
-    });
 
-    socket.on('delete message', (id) => {
-        db.run("DELETE FROM messages WHERE id = ?", id, () => io.emit('message deleted', id));
-    });
-
-    socket.on('typing', (data) => socket.broadcast.emit('user typing', data));
-});
-
-server.listen(3000, () => console.log('🔥 СЕРВЕР ЗАПУЩЕН НА ПОРТУ 3000'));
+        function checkEnter(e) { if(e.key === 'Enter') send(); }
+        function toggleMenu() { alert('Меню создания: Группы, Каналы, Контакты'); }
+    </script>
+</body>
+</html>
